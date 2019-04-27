5bf7338c87d6f5a9480dc790ee19bc99fb0d304d
Mariana <marianavargas1209@gmail.com>
2019-04-26 22:33:02 -0600

    creacion de listas

--- Proyecto2/nodo.cpp
+++ Proyecto2/nodo.cpp
@@ -4,3 +4,52 @@ Nodo::Nodo()
 {
 
 }
+
+void Nodo::setSiguiente(Nodo* siguiente){
+    this->siguiente=siguiente;
+}
+Nodo* Nodo::getSiguiente(){
+    return this->siguiente;
+}
+void Nodo::setAnterior(Nodo* anterior){
+    this->anterior=anterior;
+}
+Nodo* Nodo::getAnterior(){
+    return this->anterior;
+}
+void Nodo::setAbajo(Nodo* abajo){
+    this->abajo=abajo;
+}
+Nodo* Nodo::getAbajo(){
+    return this->abajo;
+}
+void Nodo::setArriba(Nodo* arriba){
+    this->arriba=arriba;
+}
+Nodo* Nodo::getArriba(){
+    return this->arriba;
+}
+void Nodo::setOcupadoObstaculo(bool ocupado){
+    this->ocupadoObstaculo=ocupado;
+}
+bool Nodo::getOcupadoObstaculo(){
+    return this->ocupadoObstaculo;
+}
+void Nodo::setObstaculo(Obstaculo* obstaculo){
+    this->obstaculo=obstaculo;
+}
+Obstaculo* Nodo::getObstaculo(){
+    return this->obstaculo;
+}
+void Nodo::setOcupadoGladiador(bool ocupado){
+    this->ocupadoGladiador=ocupado;
+}
+bool Nodo::getOcupadoGladiador(){
+    return this->ocupadoGladiador;
+}
+void Nodo::setGladiador(Gladiador* gladiador){
+    this->gladiador=gladiador;
+}
+Gladiador* Nodo::getGladiador(){
+    return this->gladiador;
+}
